55ef170043e7f880c765f60ae087a2f8c6c7b4e4
Vladtsrez <Competitionvladinf@gmail.com>
2023-11-02 11:51:14 +0200

    add

---
+++ src/js/order-modal.js
@@ -0,0 +1,22 @@
+const orderModal = document.getElementById('send-btn');
+const orderForm = document.getElementById('order-form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+
+orderForm.addEventListener('submit', e => {
+  e.preventDefault();
+});
+
+orderModal.addEventListener('click', () => {
+  if ((username.value === '', email.value === '')) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'The fields are filled in incorrectly or empty!',
+    });
+    username.style.outline = '2px solid #de2020';
+  } else {
+    Swal.fire('Good job!', 'Your order has been accepted!', 'success');
+    username.style.outline = '1px solid rgba(252, 249, 249, 0.1)';
+  }
+});
